4daf80daae96306184828666c5ca16841968cf01
Tristan <bilot.tristan@hotmail.fr>
2020-03-18 23:05:24 +0100

    clean tree

--- temp/hk5bo57.cpp
+++
@@ -1,9 +0,0 @@
-$.post( "compilecode", { code: code, lang: lang } )
-    .done((data) => {
-        if (data.error)
-            console.log("Error: " + data.error);
-        else if (data.output)
-            console.log("Output: " + data.output);
-        else
-            console.log("Impossible");
-    })
--- temp/us6hood/Algorithm.java
+++
@@ -1,8 +0,0 @@
-class Algorithm {
-  public static void main(String[] args) {
-    for (int i=0; i<10; i++) {
-      System.out.print(args[i] + " ");
-    }
-    System.out.println();
-  }
-}
