712c8b0068ea5fe78246f923e23d6af3a1fb855c
Tristan <bilot.tristan@hotmail.fr>
2020-03-19 21:34:08 +0100

    formatLanguage func

--- public/editor.js
+++ public/editor.js
@@ -1,37 +1,44 @@
 
 /* +++++ Editor init +++++ */
-init()
+init();
 
-const init = () => {
-    let lang = "python";
+function init() {
+    let lang = "Python";
     initEditor(lang);
 }
 
-const initEditor = (lang) => {
+function initEditor(lang) {
     if (lang == null)
         return console.log('Invalid lang !');
+    let language = formatLanguage(lang);
 
     require.config({ paths: { 'vs': './public/lib/monaco-editor/min/vs' }});
     require(['vs/editor/editor.main'], function() {
         window.editor = monaco.editor.create(document.getElementById('container'), {
             value: baseCode[lang].join('\n'),
-            language: lang
+            language: language
         });
     });
 }
 
-const updateEditor = (lang) => {
-    let language = lang.charAt(0).toLowerCase() + lang.substring(1);
-    if (lang === "C++")
-        language = "cpp";
+function updateEditor(lang) {
+    let language = formatLanguage(lang);
     let code = baseCode[lang].join('\n');
 
     window.editor.setValue(code);
     monaco.editor.setModelLanguage(window.editor.getModel(), language);
 }
 
-const getCode = () => {
+function getCode() {
     return window.editor.getValue();
+}
+
+/* Need to format the lang with good syntax (lowCase...) for editor */
+function formatLanguage(lang) {
+    let language = lang.charAt(0).toLowerCase() + lang.substring(1);
+    if (lang === "C++")
+        language = "cpp";
+    return language;
 }
 
 const baseCode = {
